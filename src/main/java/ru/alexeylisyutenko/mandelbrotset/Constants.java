6138ab20e98b4d3b6387916035b00010abbaf85e
Alexey Lisyutenko <firely3000@gmail.com>
2020-01-11 02:07:05 +0300

    Change default width and height

--- src/main/java/ru/alexeylisyutenko/mandelbrotset/Constants.java
+++ src/main/java/ru/alexeylisyutenko/mandelbrotset/Constants.java
@@ -1,8 +1,8 @@
 package ru.alexeylisyutenko.mandelbrotset;
 
 public class Constants {
-    public static final int WIDTH = 1920;
-    public static final int HEIGHT = 1080;
+    public static final int WIDTH = 1280;
+    public static final int HEIGHT = 720;
 
     public static final double LEFT = -3;
     public static final double RIGHT = 1.5;
